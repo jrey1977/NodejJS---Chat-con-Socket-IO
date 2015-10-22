99dd46b1afa3f6fc90683ebb2414b233027904b3
elnota <jrey1977@yahoo.es>
2015-10-22 11:08:50 +0200

    chat-jrey

--- models/Chat.js
+++ models/Chat.js
@@ -1,8 +1,29 @@
 var mongoose = require('mongoose'),
 Schema = mongoose.Schema;
-mongoose.connect('mongodb://localhost/test');
+//mongoose.connect('mongodb://localhost/test');
 
-var ChatSchema = new Schema({ 
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/test';
+
+// The http server will listen to an appropriate port, or default to
+// port 3000.
+var theport = process.env.PORT || 3000;
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+var ChatSchema = new Schema({
 	id: 'String',
 	username: 'String'
 });
