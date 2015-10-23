120cafad65fd0e2c9655d72077cace48c770a193
elnota <jrey1977@yahoo.es>
2015-10-23 12:35:17 +0200

    Otro commit

--- models/Chat.js
+++ models/Chat.js
@@ -3,13 +3,11 @@ var uriUtil = require('mongodb-uri'),
 Schema = mongoose.Schema;
 //mongoose.connect('mongodb://localhost/test');
 
-var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } }; 
 
 var mongodbUri = 'mongodb://heroku_gqb3tr50:r22sg8c10850si7utsm4e3nj40@ds041494.mongolab.com:41494/heroku_gqb3tr50';
 var mongooseUri = uriUtil.formatMongoose(mongodbUri);
 
-mongoose.connect(mongooseUri, options);
+mongoose.connect(mongooseUri);
 
 var db = mongoose.connection;
 
